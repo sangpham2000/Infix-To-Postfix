09583695ebd1c302bf91eb8989ffd895d960942c
sangpham2000 <46708390+sangpham2000@users.noreply.github.com>
2022-04-07 19:37:19 +0700

    Update Requirement2_OutputGetter.java

--- Requirement2_OutputGetter.java
+++ Requirement2_OutputGetter.java
@@ -1,4 +1,3 @@
-//Sinh vien tuyet doi khong duoc sua file nay
 public interface Requirement2_OutputGetter {
 	public void setInputString(String inputString);
 	public double getResultOfExpression();
