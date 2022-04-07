6a78f4953f9ed9b27fef23343fb7368295a6c340
sangpham2000 <46708390+sangpham2000@users.noreply.github.com>
2022-04-07 19:35:14 +0700

    Update CalculatePostfixSolve.java

--- CalculatePostfixSolve.java
+++ CalculatePostfixSolve.java
@@ -1,34 +1,31 @@
 import java.util.Stack;
 
-//Sinh vien khong chinh sua bat ky dong lenh nao ben tren dong comment "Phan bai lam cua sinh vien", duoc phep them thu vien khi can thiet
-
 public class CalculatePostfixSolve implements Requirement2_OutputGetter{
 	private String inputString;
 	private double resultOfExpression;
 	
-	public CalculatePostfixSolve(){ //Phuong thuc khong duoc chinh sua
+	public CalculatePostfixSolve(){ 
 		this.inputString = "";
 		this.resultOfExpression = 0;
 	}
 	
-	public CalculatePostfixSolve(String inputString){ //Phuong thuc khong duoc chinh sua
+	public CalculatePostfixSolve(String inputString){
 		this.inputString = inputString;
 		this.resultOfExpression = 0;
 	}
 	
-	public void setInputString(String inputString){ //Phuong thuc khong duoc chinh sua
+	public void setInputString(String inputString){ 
 		this.inputString = inputString;
 	}	
 	
-	public double getResultOfExpression(){ //Phuong thuc khong duoc chinh sua
+	public double getResultOfExpression(){ 
 		this.resultOfExpression = calculatePostfix();
 		return this.resultOfExpression;
 	}	
 	
-	// Phan bai lam cua sinh vien
+
 	
 	private double calculatePostfix(){
-        // code here
 		String[] str = stringTokenizer(inputString);
 		Stack<Double> stack = new Stack<Double>();
 		double cal = 0;
@@ -75,7 +72,6 @@ public class CalculatePostfixSolve implements Requirement2_OutputGetter{
 	}
 	
 	private String[] stringTokenizer(String str){
-        // code here
 		String[] tokens = new String[str.length()];
 		int n = 0;
 		for(int i = 0; i < str.length();i++)
